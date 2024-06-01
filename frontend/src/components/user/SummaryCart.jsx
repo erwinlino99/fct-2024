01c5967dfafa4bbe3d898f4aede001fb5fc90360
erwinLino <erwinlino011@gmail.com>
2024-06-01 22:22:39 +0200

    Logica casi terminada, falta: Quitar productos segun el numero de cantidad y hacer pdf

--- frontend/src/components/user/SummaryCart.jsx
+++ frontend/src/components/user/SummaryCart.jsx
@@ -1,15 +1,16 @@
 import { Button, Typography } from "@mui/material";
 import { useState, useEffect } from "react";
 import { Divider } from "@mui/material";
-
-const SummaryCart = ({ items }) => {
+import { useNavigate } from "react-router-dom";
+const SummaryCart = ({ items, pdf }) => {
+  const navigate = useNavigate();
   const [price, setPrice] = useState(0);
   const delivery = 7.99;
   const discountRate = 10;
   const calculatePrice = (items) => {
     let total = 0;
     for (let item of items) {
-      total += item.price;
+      total += item.price * item.times;
     }
 
     setPrice(total);
@@ -17,6 +18,7 @@ const SummaryCart = ({ items }) => {
 
   useEffect(() => {
     calculatePrice(items);
+    //console.log('dentro del carrito-->',items)
   }, [items]);
 
   return (
@@ -33,11 +35,25 @@ const SummaryCart = ({ items }) => {
       <Typography variant="h6">
         Total: {(price - 10 * (price / 100)).toFixed(2)}{" "}
       </Typography>
-      <Button variant="contained" color="success">
-        Comprar
+
+      {pdf == false ? (
+        <Button
+          variant="contained"
+          color="success"
+          onClick={() => navigate("/payment")}
+        >
+          Comprar
+        </Button>
+      ) : (
+        <Button
+        variant="contained"
+        color="success"
+      >
+        Finalizar compra
       </Button>
+      )}
     </div>
   );
 };
-
+// {`../img/${prod.id}.jpg`}
 export default SummaryCart;
---
+++ frontend/src/img/images.js
@@ -0,0 +1,17 @@
+import pantalonMujer from "./2.jpg";
+import ZapatillasBoy from "./3.jpg";
+import VestidoMujer from "./4.jpg";
+import camisaHombre from "./5.jpg";
+import pantalonCorto from "./9.jpg";
+import corbataNegra from "./10.jpg";
+
+const path = {
+  2: pantalonMujer,
+  3: ZapatillasBoy,
+  4: VestidoMujer,
+  5: camisaHombre,
+  9: pantalonCorto,
+  10: corbataNegra,
+};
+
+export default path;
