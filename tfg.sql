47ee81107ce1a6a798a9cebe133788bffaa9e7bb
erwinLino <erwinlino011@gmail.com>
2024-06-10 01:01:06 +0200

    addicion

--- tfg.sql
+++ tfg.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-06-2024 a las 00:55:02
+-- Tiempo de generación: 10-06-2024 a las 00:57:41
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -43,12 +43,18 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `description`, `category`, `price`, `stock`) VALUES
-(2, 'Pantalones Mujer', 'Pantalón tobillero sin cierre', 'Mujer', 100, 30),
-(3, 'Zapatillas Niño', 'Zapatillas deportivas de malla', 'boys', 29.99, 20),
+(2, 'Pantalones Mujer', 'Pantalón tobillero sin cierre', 'Mujer', 100, 845),
+(3, 'Zapatillas Niño', 'Zapatillas deportivas de malla', 'boys', 29.99, 938),
 (4, 'Vestido Mujer', 'Vestido de tirantes en punto texturizado', 'Mujer', 49.99, 15),
-(5, 'Camisa Hombre', 'Camisa resort en mezcla de lino Regular Fit', 'hombre', 24.99, 40),
-(9, 'Pantalon Corto', 'Pantalón corto en mezcla de lino', 'boys', 39.99, 32),
-(10, 'Corbata Negra', 'Corbata de satén', 'hombre', 39.99, 32);
+(5, 'Camisa Hombre', 'Camisa resort en mezcla de lino Regular Fit', 'hombre', 24.99, 31),
+(9, 'Pantalon Corto', 'Pantalón corto en mezcla de lino', 'boys', 39.99, 28),
+(10, 'Corbata Negra', 'Corbata de satén', 'hombre', 39.99, 23),
+(11, 'Gafas Sol', 'Gafas rectangulares', 'accesorios', 8, 2),
+(12, 'Bandana', 'Banda azul patron', 'accesorios', 12, 7),
+(13, 'Sombrero', 'Sombrero cowboy', 'accesorios', 33, 12),
+(14, 'Vestido Rib', 'Vestido Rojo', 'girls', 21, 6),
+(15, 'Top Rib', 'Top rojo', 'girls', 17, 21),
+(16, 'Camiseta verano', 'Camiseta Maggie Simpson', 'girls', 8, 11);
 
 -- --------------------------------------------------------
 
@@ -74,9 +80,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `surname`, `password_hash`, `domicile`, `bornDate`, `phone`, `email`, `wishlist`, `cart`) VALUES
-(1, 'Erwin', 'Lino Zabala', '123', 'C/MisCos', '1999-03-22', 643040866, 'user1@example.com', '[{\"category\": \"hombre\", \"description\": \"Corbata de sat\\u00e9n\", \"id\": 10, \"name\": \"Corbata Negra\", \"price\": 39.99, \"stock\": 32}, {\"category\": \"hombre\", \"description\": \"Camisa resort en mezcla de lino Regular Fit\", \"id\": 5, \"name\": \"Camisa Hombre\", \"price\": 24.99, \"stock\": 40}]', '[{\"category\": \"Mujer\", \"description\": \"\", \"id\": 2, \"name\": \"Pantalones Mujer\", \"price\": 100, \"stock\": 30, \"times\": 5}, {\"category\": \"boys\", \"description\": \"\", \"id\": 3, \"name\": \"Zapatillas Ni\\u00f1o\", \"price\": 29.99, \"stock\": 20, \"times\": 2}]'),
+(1, 'Erwin', 'Lino Zabala', 'editado', 'Calle EDITADA', '1999-03-02', 9999, 'user1@example.com', '[{\"category\": \"hombre\", \"description\": \"Camisa resort en mezcla de lino Regular Fit\", \"id\": 5, \"name\": \"Camisa Hombre\", \"price\": 24.99, \"stock\": 31}, {\"category\": \"hombre\", \"description\": \"Corbata de sat\\u00e9n\", \"id\": 10, \"name\": \"Corbata Negra\", \"price\": 39.99, \"stock\": 23}]', '[{\"category\": \"boys\", \"description\": \"Pantal\\u00f3n corto en mezcla de lino\", \"id\": 9, \"name\": \"Pantalon Corto\", \"price\": 39.99, \"stock\": 28, \"times\": 1}, {\"category\": \"boys\", \"description\": \"Zapatillas deportivas de malla\", \"id\": 3, \"name\": \"Zapatillas Ni\\u00f1o\", \"price\": 29.99, \"stock\": 938, \"times\": 1}]'),
 (8, 'prueba', '', 'prueba', 'C/Hola', '1999-03-22', 123456789, 's@gmail.com', '[]', '[]'),
-(9, 'Angel', '', 'angel', NULL, NULL, 0, 'angel@gmail.com', '[]', '[{\"category\": \"hombre\", \"description\": \"Camisa resort en mezcla de lino Regular Fit\", \"id\": 5, \"name\": \"Camisa Hombre\", \"price\": 24.99, \"stock\": 40, \"times\": 2}, {\"category\": \"hombre\", \"description\": \"Corbata de sat\\u00e9n\", \"id\": 10, \"name\": \"Corbata Negra\", \"price\": 39.99, \"stock\": 32, \"times\": 2}, {\"category\": \"boys\", \"description\": \"Pantal\\u00f3n corto en mezcla de lino\", \"id\": 9, \"name\": \"Pantalon Corto\", \"price\": 39.99, \"stock\": 32, \"times\": 1}]');
+(10, 'nuevo', '', 'nuevo', 'C/ Nueva', '1999-03-22', 643040865, 'nuevo@gmail.com', '[]', '[]');
 
 --
 -- Índices para tablas volcadas
@@ -104,13 +110,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
